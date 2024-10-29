cafc1dc13632f5e47205aea3088e132081b9ca49
Melissa McMurtrie <melissa@melissakate.com>
2024-10-29 14:07:33 -0400

    adding mkm chat

--- main.py
+++ main.py
@@ -20,6 +20,15 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 chat_responses = []
+mkm_responses = []
+
+
+def is_valid_json(string):
+    try:
+        json.loads(string)
+    except ValueError:
+        return False
+    return True
 
 
 def get_path_based_on_env():
@@ -36,11 +45,9 @@ def get_path_based_on_env():
 path = get_path_based_on_env()
 
 
-def write_chat_log_to_file(chat_set):
-    with open(path + 'chat_logs/data.json', 'r') as f:
+def write_chat_log_to_file(chat_set, filename):
+    with open(path + 'chat_logs/' + filename + '.json', 'r') as f:
         data_list = json.load(f)
-
-    print(data_list)
 
     # Check if the loaded data is a list
     if isinstance(chat_set, list):
@@ -48,13 +55,20 @@ def write_chat_log_to_file(chat_set):
     else:
         data_list.append(chat_set)
 
-    with open(path + 'chat_logs/data.json', 'w', encoding='utf-8') as f:
+    with open(path + 'chat_logs/' + filename + '.json', 'w', encoding='utf-8') as f:
         json.dump(data_list, f, ensure_ascii=False, indent=4)
 
 
 @app.get("/", response_class=HTMLResponse)
 async def chat_page(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": chat_responses})
+    chat_responses = []
+    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": []})
+
+
+@app.get("/mkm", response_class=HTMLResponse)
+async def mkm_chat_page(request: Request):
+    mkm_responses = []
+    return templates.TemplateResponse("home.html", {"request": request, "chat_responses": []})
 
 
 chat_log = [{'role': 'system',
@@ -71,8 +85,22 @@ async def chat(websocket: WebSocket):
 
     while True:
         user_input = await websocket.receive_text()
-        chat_log.append({'role': 'user', 'content': user_input})
-        chat_responses.append(user_input)
+        user_message = user_input
+        is_mkm = False
+        datafile = 'data'
+
+        if is_valid_json(user_input):
+            user_json = json.loads(user_input)
+            user_message = user_json['message']
+            print(user_message)
+            datafile = 'mkm-data'
+            is_mkm = True
+
+        chat_log.append({'role': 'user', 'content': user_message})
+        if is_mkm:
+            mkm_responses.append(user_message)
+        else:
+            chat_responses.append(user_message)
 
         try:
             response = openai.chat.completions.create(
@@ -88,12 +116,16 @@ async def chat(websocket: WebSocket):
                 if chunk.choices[0].delta.content is not None:
                     buffer += chunk.choices[0].delta.content
             await websocket.send_text(buffer)
-            chat_responses.append(buffer)
+            if is_mkm:
+                mkm_responses.append(buffer)
+            else:
+                chat_responses.append(buffer)
+            chat_log.append({'role': 'assistant', 'content': buffer})
             data_set = {
-                'user': user_input,
+                'user': user_message,
                 'assistant': buffer
             }
-            write_chat_log_to_file(data_set)
+            write_chat_log_to_file(data_set, datafile)
 
         except Exception as e:
             await websocket.send_text(f'Error: {str(e)}')
