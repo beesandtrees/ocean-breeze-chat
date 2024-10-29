e4f618497d9165893a1ea6b8c87173dd658ee444
Melissa McMurtrie <melissamcmurtrie@melissas-ideapad.home>
2024-10-29 10:11:07 -0400

    logging chats

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from openai import OpenAI
 from fastapi import FastAPI, Form, Request, WebSocket
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 from typing import Annotated
 import os
-import uvicorn
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -14,10 +16,27 @@ openai = OpenAI(
 )
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 chat_responses = []
+
+
+def write_chat_log_to_file(chat_set):
+    with open('./chat_logs/data.json', 'r') as f:
+        data_list = json.load(f)
+
+    print(data_list)
+
+    # Check if the loaded data is a list
+    if isinstance(chat_set, list):
+        data_list.extend(chat_set)
+    else:
+        data_list.append(chat_set)
+
+    with open('./chat_logs/data.json', 'w', encoding='utf-8') as f:
+        json.dump(data_list, f, ensure_ascii=False, indent=4)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -57,6 +76,11 @@ async def chat(websocket: WebSocket):
                     buffer += chunk.choices[0].delta.content
             await websocket.send_text(buffer)
             chat_responses.append(buffer)
+            data_set = {
+                'user': user_input,
+                'assistant': buffer
+            }
+            write_chat_log_to_file(data_set)
 
         except Exception as e:
             await websocket.send_text(f'Error: {str(e)}')
